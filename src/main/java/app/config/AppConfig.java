33a3ca4e5834ef69fc233ce7c2a953c5e36a3325
zuryanov <zuryanovalexei@gmail.com>
2020-08-07 20:01:22 +0500

    2.1.3

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,16 @@
 package app.config;
 
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+    @Bean
+    public Timer getTimer() {
+        return new Timer();
+    }
 }
